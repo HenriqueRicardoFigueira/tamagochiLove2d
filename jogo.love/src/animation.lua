a95ae0962efe6bf2ebee264659480d629bf7397f
Henrique Figueira <39686167+HenriqueRicardoFigueira@users.noreply.github.com>
2019-06-21 22:18:21 -0300

    Update animations

--- jogo.love/src/animation.lua
+++ jogo.love/src/animation.lua
@@ -19,7 +19,14 @@ function Anim:reset()
     self.time = 1
 end
 
+function Anim:set(quad)
+    self:reset()
+    quad:setViewport(self.offset.x, self.offset.y, self.size.x, self.size.y)
+end
+
 function Anim:update(dt, quad)
+    if(self.num_frames <= 1) then return end
+
     self.timer = self.timer - dt
     if(self.timer <= 0) then
         self.timer = 1 / self.fps
@@ -27,9 +34,9 @@ function Anim:update(dt, quad)
         if self.frame > self.num_frames then
             self.frame = 1
         end
-        self.offset.x = self.start_offset.x + (self.size.x * (self.frame - 1))
-        self.offset.y = self.start_offset.y + (self.size.y * math.floor( self.frame / self.column_size ))
-        quad:setViewport(self.offset.x, self.offset.y, self.size.x, self.size.y)
+        self.offset.x = self.start_offset.x + (self.size.x * ((self.frame - 1) % (self.column_size)))
+        self.offset.y = self.start_offset.y + (self.size.y * math.floor( (self.frame - 1) / self.column_size ))
+        self.set(quad)
     end
 end
 

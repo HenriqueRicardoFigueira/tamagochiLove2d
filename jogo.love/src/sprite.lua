a9a04c105fa15ea567459a535af3aaa9a4fb2631
Henrique Figueira <39686167+HenriqueRicardoFigueira@users.noreply.github.com>
2019-06-21 23:48:03 -0300

    fixed bugs

--- jogo.love/src/sprite.lua
+++ jogo.love/src/sprite.lua
@@ -1,21 +1,40 @@
-local class = require("class")
+local class = require("src/class")
 local Sprite = class:derive("Sprite")
+local Anim = require("src/animation")
 
 
-function Sprite:new()
+function Sprite:new(spriteX, w, h, x, y)
+    self.w = w
+    self.h = h
+    self.x = x
+    self.y = y
+    self.spriteX = spriteX
     self.animations = {}
+    self.current_anim = ""
+    self.quad = love.graphics.newQuad(0, 0, w, h, spriteX:getDimensions())
 end
 
 function Sprite:animate(anim_name)
-    -- body
+    if self.current_anim ~= anim_name and self.animations[anim_name] ~= nil then
+        self.current_anim = anim_name
+        self.animations[anim_name]:reset()
+        self.animations[anim_name]:set(self.quad)
+       
+    end
+end
+
+function Sprite:add_animation(name, anim)
+    self.animations[name] = anim
 end
 
 function Sprite:update(dt)
-    -- body
+    if self.animations[self.current_anim] ~= nil then
+        self.animations[self.current_anim]:update(dt, self.quad)
+    end
 end
 
 function Sprite:draw()
-    -- body
+    love.graphics.draw(self.spriteX, self.quad, self.x , self.y, 0, 1, 1, self.w / 2, self.h / 2)
 end
 
 return Sprite

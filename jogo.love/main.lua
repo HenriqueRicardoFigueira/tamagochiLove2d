6708d5f663df8c201e082726e3d31b23ed8a0645
Henrique Figueira <39686167+HenriqueRicardoFigueira@users.noreply.github.com>
2019-06-26 20:56:30 -0300

    use life bar

--- jogo.love/main.lua
+++ jogo.love/main.lua
@@ -6,6 +6,7 @@ local suit = require 'suit'
 local Player = require("src/player")
 local Pet = require("src/pet")
 local p1 = Pet("gayarados")
+local ui_bar = require("src/bars")
 
 -- criação de botão
 local slider = {value = 1, max = 2}
@@ -15,12 +16,19 @@ local slider = {value = 1, max = 2}
 local charp = Anim(130, 0, 134, 84, 6, 6, 3)
 local charv = Anim(123, 95, 134, 84, 6, 6, 3)
 local charc = Anim(108, 240, 108, 87, 7, 7, 3)
-local ss  
+local ssV
+local b1
+local b2  
 
 function love.load()
     love.graphics.setDefaultFilter('nearest', 'nearest')
     image_charV = love.graphics.newImage("graphics/charNv.png")
-    background = love.graphics.newArrayImage("graphics/bg.png")
+    background = love.graphics.newImage("graphics/bg.png")
+    bar1 = love.graphics.newImage("graphics/bar_1.png")
+    bar2 = love.graphics.newImage("graphics/bar_2.png")
+    --barras
+    bVida1 = ui_bar(bar1, 600, 60, math.rad(0), 0.1, 0.1, 100, 100)
+    bVida2 = ui_bar(bar2, 600, 60, math.rad(0), 0.1, 0.1, 100, 100)
     --      (spriteX, w, h, x, y, sx, sy)
     ssV = Spr(image_charV, 134, 84, 400, 400, 3, 3)
     ssV:add_animation("charizardV", charv)
@@ -34,10 +42,11 @@ function love.update(dt)
     if dt > 0.035 then return end
 
     ssV:update(dt)
-    
-    
+     
+
     if suit.Button("Alimentar", 50, 50, 80, 80).hit then
         p1:toFeed()
+        bVida2:upScale((p1.hunger/1000))
         --ssV:animate("charizardC")
     end
     
@@ -50,6 +59,10 @@ end
 
 function love.draw()
     love.graphics.draw(background)
+    --love.graphics.draw(bar1, 600, 60, math.rad(0), 0.1, 0.1, 100, 100)
+    --love.graphics.draw(bar2, 600, 60, math.rad(0), 0.06, 0.1, 100, 100)
+    bvida1:draw()
+    bvida2:draw() 
     suit.draw()
     ssV:draw()
     suit.Label(p1.hunger, {align="left"}, 0, 150, 200, 30)   

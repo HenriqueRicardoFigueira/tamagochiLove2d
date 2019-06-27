cddf9aaae838a608c05882a493ee934fe06a5fd3
Henrique Figueira <39686167+HenriqueRicardoFigueira@users.noreply.github.com>
2019-06-27 17:05:59 -0300

    fixed main

--- jogo.love/main.lua
+++ jogo.love/main.lua
@@ -1,16 +1,33 @@
 -- import de classes
 local g = require("src/game")
-
+local s = require("suit")
+local gameStatus = "menu"
+local menu = require("src/menu")
 
 function love.load()
-   game = g()
+    game = g()
+    menu:load()
+    background = love.graphics.newImage("graphics/bg.png")
 end
 
 function love.update(dt)
-   game:update(dt)
+    game:update(dt)
+    
+    gameStatus = menu:checkGoing()
+    if gameStatus == "start" then
+        gameStatus = "game"
+    end
+
+
 end
 
 function love.draw()
-    game:draw()
+    love.graphics.draw(background);
+    --love.graphics.setBackgroundColor(255, 0, 0)
+    --love.graphics.print(gameStatus, 400, 80)
+    
+    if gameStatus == "menu" then menu:draw() end
+
+    if gameStatus == "game" then game:draw() end
 end
 
